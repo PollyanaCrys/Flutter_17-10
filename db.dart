e71f1922ed87e81fbb074092d8aa168d0d719994
PollyanaCrys <103380013+PollyanaCrys@users.noreply.github.com>
2022-10-17 13:17:37 -0300

    Create db.dart

---
+++ db.dart
@@ -0,0 +1,56 @@
+import 'cafe.dart';
+
+class DB {
+  static List<Cafe> lista = [
+    Cafe(
+        id: 1,
+        foto:
+        "https://www.supremoarabica.com.br/wp-content/uploads/2019/04/10-curisoidades-sobre-o-caf%C3%A9.jpg",
+        nome: "Café",
+        aux: "Tradicional",
+        volume: "150",
+        preco: "8,00",
+        ingredientes: "Café extra-forte, açucar..."),
+    Cafe(
+        id: 2,
+        foto:
+        "https://www.supremoarabica.com.br/wp-content/uploads/2019/04/10-curisoidades-sobre-o-caf%C3%A9.jpg",
+        nome: "Café",
+        aux: "Tradicional",
+        volume: "150",
+        preco: "8,00",
+        ingredientes: "Café extra-forte, açucar..."),
+    Cafe(
+        id: 3,
+        foto:
+        "https://www.supremoarabica.com.br/wp-content/uploads/2019/04/10-curisoidades-sobre-o-caf%C3%A9.jpg",
+        nome: "Café",
+        aux: "Tradicional",
+        volume: "150",
+        preco: "8,00",
+        ingredientes: "Café extra-forte, açucar..."),
+    Cafe(
+        id: 4,
+        foto:
+        "https://www.supremoarabica.com.br/wp-content/uploads/2019/04/10-curisoidades-sobre-o-caf%C3%A9.jpg",
+        nome: "Café",
+        aux: "Tradicional",
+        volume: "150",
+        preco: "8,00",
+        ingredientes: "Café extra-forte, açucar..."),
+    Cafe(
+        id: 5,
+        foto:
+        "https://www.supremoarabica.com.br/wp-content/uploads/2019/04/10-curisoidades-sobre-o-caf%C3%A9.jpg",
+        nome: "Café",
+        aux: "Tradicional",
+        volume: "150",
+        preco: "8,00",
+        ingredientes: "Café extra-forte, açucar..."),
+  ];
+
+  static Future<List<Cafe>> getValues() async {
+    await Future.delayed(const Duration(seconds: 4));
+    return lista;
+  }
+}
